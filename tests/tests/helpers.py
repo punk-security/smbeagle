cc58b8f261a5dcbeddb3a53f3d3bf490a6729c6b
Simon Gurney <simon.gurney@punksecurity.co.uk>
2022-07-16 23:41:13 +0100

    feat: add tests using impacket

---
+++ tests/tests/helpers.py
@@ -0,0 +1,68 @@
+from impacket import smbserver
+import multiprocessing
+import os
+import subprocess
+from time import sleep
+import csv
+import shutil
+import uuid
+
+
+def __setupSMB(address, dir, SMB2 = True):
+    os.chdir("/empty_dir")
+    server = smbserver.SimpleSMBServer(listenAddress=address, listenPort=445)
+    server.addShare("share", dir, "")
+    server.addCredential("test", 1200, "9FD78381EC915F1AAAD3B435B51404EE", "25EDEDFF26CB970623DDA4733227A3F7")
+    server.setSMB2Support(SMB2)
+    server.start()
+
+def setupSMB(address, dir):
+    process = multiprocessing.Process(target=__setupSMB, args=[address, dir])
+    process.start()
+    return process
+
+class SMB(object):
+    def __init__(self, address = "0.0.0.0", dir_structure = ["fileA", "fileB"]):
+        self.address = address
+        self.dir_structure = dir_structure
+        self.dir = f"/{uuid.uuid4().hex}"
+    def __enter__(self):
+        self.smb = setupSMB(self.address, self.dir)
+        os.mkdir(self.dir)
+        self.populate_dir(self.dir, self.dir_structure)
+    def populate_dir(self, dir, dir_structure):
+        for item in dir_structure:
+            if type(item) != type( () ) and type(item) != type(""):
+                raise ValueError("Directory should be list of strings and tuples")
+            if type(item) == type( () ):
+                #type tuple, so create folder and then parse that structure
+                os.mkdir(f"{dir}{os.sep}{item[0]}")
+                self.populate_dir(f"{dir}{os.sep}{item[0]}", item[1])
+            else:
+                # type string, so make the file
+                open(f"{dir}{os.sep}{item}", 'a').close()
+
+    def __exit__(self, *args, **kwargs):
+        self.smb.kill()
+        shutil.rmtree(self.dir)
+        #self.smb.close()
+
+def runSMBeagle(*args, print_out=True):
+    run = subprocess.run(["smbeagle",*args], stdout = subprocess.PIPE, universal_newlines=True)
+    if print_out:
+        print(run.stdout)
+    return run.stdout
+
+def runSMBeagleToCSV(*args):
+    return runSMBeagle("-c","out.csv",*args)
+
+def runSMBeagleToCSVWithAuth(*args):
+    return runSMBeagleToCSV("-u","test", "-p", "goose", *args)
+
+def runSMBeagleToCSVWithAuthAndReturnResults(*args):
+    print(runSMBeagleToCSVWithAuth(*args))
+    with open('out.csv', newline='') as csvfile:
+        results = list(csv.DictReader(csvfile, delimiter=',', quotechar='"'))
+        for result in results:
+            print(result)
+        return results
---
+++ tests/tests/test_tcp_scans.py
@@ -0,0 +1,61 @@
+from helpers import *
+
+smb_reachable_message = "we have {} hosts with reachable SMB services"
+
+no_smb_service_discovered_message = smb_reachable_message.format(0)
+one_smb_service_discovered_message = smb_reachable_message.format(1)
+two_smb_service_discovered_message = smb_reachable_message.format(2)
+three_smb_service_discovered_message = smb_reachable_message.format(3)
+four_smb_service_discovered_message = smb_reachable_message.format(4)
+
+def test_one_manual_host_tcp_success():
+    with SMB():
+        assert one_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-h", "127.0.0.2")
+
+def test_one_manual_host_tcp_fail_if_not_listening():
+    with SMB("127.0.0.2"):
+        assert no_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-h", "127.0.0.3")
+
+def test_two_manual_host_tcp_success():
+    with SMB("127.0.0.2"):
+        with SMB("127.0.0.3"):
+            assert two_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-h", "127.0.0.2", "127.0.0.3")
+
+def test_one_manual_host_tcp_success_and_not_two_if_second_not_listening():
+    with SMB("127.0.0.2"):
+            assert one_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-h", "127.0.0.2", "127.0.0.3")
+
+def test_one_discovered_host_tcp_success():
+    with SMB("127.0.0.2"):
+        assert one_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-n", "127.0.0.0/24")
+
+def test_no_discovered_host_when_filtered():
+    with SMB("127.0.0.2"):
+        assert no_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-n", "127.0.0.0/24","-H","127.0.0.2" )
+
+def test_one_discovered_host_when_one_filtered():
+    with SMB("127.0.0.2"):
+        with SMB("127.0.0.3"):
+            assert one_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-n", "127.0.0.0/24","-H","127.0.0.2" )
+
+def test_two_discovered_host_tcp_success():
+    with SMB("127.0.0.2"):
+        with SMB("127.0.0.3"):
+            assert two_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-n", "127.0.0.0/24")
+
+def test_three_discovered_host_tcp_success():
+    with SMB("127.0.0.2"):
+        with SMB("127.0.0.3"):
+            with SMB("127.0.0.4"):
+                assert three_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-n", "127.0.0.0/24")
+
+def test_four_discovered_host_tcp_success():
+    with SMB("127.0.0.2"):
+        with SMB("127.0.0.3"):
+            with SMB("127.0.0.4"):
+                with SMB("127.0.0.5"):
+                    assert four_smb_service_discovered_message in runSMBeagleToCSVWithAuth("-n", "127.0.0.0/24")
+
+def test_disable_network_discovery():
+    no_networks_to_scan_message = "there are no networks or hosts to scan"
+    assert no_networks_to_scan_message in runSMBeagleToCSVWithAuth("-D")
